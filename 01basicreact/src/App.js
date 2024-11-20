4571eeb25539a2a4aaac5b343220b72dba96a609
hinzabajwa1 <hinzabajwa@gmail.com>
2024-11-20 20:18:35 +0500

    Add new React projects

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+   <h1>React with basicreact|Hinza</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+function App() {
+ 
+
+  return (
+   <h1>React with Vite | Hinza</h1>
+  )
+}
+
+export default App
